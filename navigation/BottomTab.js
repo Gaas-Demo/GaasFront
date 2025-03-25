f2723010909cf1f96508c4216834995d7d67a210
Ville Niemi <vipeni35@gmail.com>
2025-03-25 11:44:13 +0200

    styling

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -1,11 +1,20 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { DrawerToggleButton } from '@react-navigation/drawer';
+import { Icon } from 'react-native-paper';
 import PlaceholderScreen from '../screens/Placeholder';
 import Map from '../screens/Map';
+import Homepage from '../screens/HomepageComponent';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
+
+    const options={
+        headerStyle: { backgroundColor: "#FF4242" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { fontWeight: "bold" },
+    }
+
     return (
         <Tab.Navigator
             screenOptions={{
@@ -15,10 +24,12 @@ export default function BottomTabNavigator() {
                     <DrawerToggleButton />
                 )
             }}>
-            <Tab.Screen name="Placeholder1" component={PlaceholderScreen} />
+            <Tab.Screen name="Your Contracts" component={Homepage} options={options}/>
+            <Tab.Screen name="Browse" component={PlaceholderScreen} />
             <Tab.Screen name="Map" component={Map} />
             {/* <Tab.Screen name="Page name here" component={Component name here} /> */}
         </Tab.Navigator>
     );
 }
+
 
