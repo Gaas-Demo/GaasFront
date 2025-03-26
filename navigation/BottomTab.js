b08e1031149332c44d8a539983f7aaf3965a5afd
JanTPau <127750707+JanTPau@users.noreply.github.com>
2025-03-26 11:22:55 +0200

    Home fuel gauge and rough no-car-selected screen

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { DrawerToggleButton } from '@react-navigation/drawer';
 import PlaceholderScreen from '../screens/Placeholder';
 import Map from '../screens/Map';
+import Homepage from '../screens/HomepageComponent';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,7 +16,7 @@ export default function BottomTabNavigator() {
                     <DrawerToggleButton />
                 )
             }}>
-            <Tab.Screen name="Placeholder1" component={PlaceholderScreen} />
+            <Tab.Screen name="Home" component={Homepage} />
             <Tab.Screen name="Map" component={Map} />
             {/* <Tab.Screen name="Page name here" component={Component name here} /> */}
         </Tab.Navigator>
